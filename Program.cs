a29af6e917be65bb7b2712c3c6ca5881abc241c9
NathanaelLara <loshierro2000@gmail.com>
2025-04-30 23:49:32 -0400

    Primer commit

---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.Data.SqlClient;
+using Microsoft.Identity.Client;
+using Microsoft.Win32.SafeHandles;
+
+// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, World!");
+
+string connString = "Server=localhost;Database=Northwind;User Id=Sa;Password=Sa123456;TrustServerCertificate=True";
+
+Console.WriteLine("Conexion Abierta");
+
+using (var conn = new SqlConnection(connString))
+{
+    try
+    {
+        conn.Open();
+        string query = "SELECT * FROM employees";
+
+        using (SqlCommand cmd = new SqlCommand(query, conn))
+        {
+            cmd.Parameters.AddWithValue("@Edad", "34");
+
+            using (SqlDataReader reader = cmd.ExecuteReader())
+            {
+                while (reader.Read())
+                {
+                    Console.WriteLine($"{reader["FirstName"]});
+                    Console.WriteLine($"{reader["LastName"]});
+                }
+            }
+        }
+    }
+    catch (Exception ex)
+    {
+        Console.WriteLine(ex.Message);
+    }
+}
